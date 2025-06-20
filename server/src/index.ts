afcdfb2e5c32ceecbf513eed8d467e89177a9885
MyLookUpDev <oussama.ameziane.dev@gmail.com>
2025-06-20 19:07:50 +0100

    Updated Home page design and images

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,8 @@ import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
+void authRoutes;
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || '';
